6f7d4e6dffdbef27b03ff42fe368adad2cd93ddf
alqahtanisarra <sarra.alqahtani@gmail.com>
2020-02-18 14:51:50 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Faculty{
+
+    string name;
+    string id;
+    string department;
+    string degree;
+
+};
+
+int main() {
+    Faculty prof;
+
+    return 0;
+}
+
